5605c914ce22d70e2208a9e6dfeac9e427e0c77f
umikami <ggoku02@gmail.com>
2022-07-30 21:29:03 -0400

    initial skeleton for home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Container, Row, Col } from "react-bootstrap"
+import CallToActionText from '../components/CallToActionText';
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+const Home: React.FC = () => {
+  const linkText: string = "check out my work";
+
+  return <Container className="main-container">
+    <Row>
+      <Col >
+        <h1 className="home__mainHeader">
+          {"< Hello there! I am Ernesto />"}
+        </h1>
+        <p className="home__subHeader">
+          {"std::cout << “A Full-Stack Software Developer” << endl;"}
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <CallToActionText beforeLinkText="" linkText={linkText} afterLinkText=""/>
+      </Col>
+    </Row>
+  </Container>
 }
 
 export default Home
