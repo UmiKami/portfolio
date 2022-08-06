eb07269c4e3bdbb04fa8b1133fe363438179d5ff
umikami <ggoku02@gmail.com>
2022-08-06 15:42:21 -0400

    made navbar mobile responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,20 @@ REMOVE PHONE NUMBER FROM RESUME
 =====IMPORTANT=====================
 */
 import { Link } from "react-router-dom"
+import useWindowDimensions from '../Hooks/WindowDimensions';
 
 const Navbar = () => {
+  const {width} = useWindowDimensions()
+
   return <>
     <nav className="navbar navbar-expand-lg bg-transparent">
-      <div className="container nav-container">
-        <Link className="navbar-brand" to={"/"}>Ernesto Gonzalez</Link>
+      <div className="container nav-container position-relative">
+        {width > 991 ? <Link className="navbar-brand" to={"/"}>Ernesto Gonzalez</Link> : ""}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="nav  gap-4">
+        <div className="collapse navbar-collapse nav-compact" id="navbarNav">
+          <ul className="nav gap-4 nav-list">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/projects">Projects</Link>
             </li>
